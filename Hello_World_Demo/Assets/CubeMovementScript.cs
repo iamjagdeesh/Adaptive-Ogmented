64945b084a989c2aba817a65c6e38b55f0f23246
Aditya C <aditya.chayapathy@gmail.com>
2018-11-07 21:59:45 -0700

    Adding demo project

---
+++ Hello_World_Demo/Assets/CubeMovementScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovementScript : MonoBehaviour {
+	Vector3 temp = new Vector3(1,0,0);
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += temp * Time.deltaTime;
+	}
+}
