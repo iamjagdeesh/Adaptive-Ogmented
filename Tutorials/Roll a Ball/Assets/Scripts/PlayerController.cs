a00f93c148f85c55d09ae3551cf40fcc9aa6bc18
jagdeesh-basavaraju <jagdeesh.basavaraju@fireeye.com>
2018-11-10 04:19:34 -0700

    Small roll a ball game completed.

--- Tutorials/Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Tutorials/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -8,8 +9,17 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 
+	private int count;
+
+	public Text countText;
+
+	public Text winText;
+
 	void Start() {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	void FixedUpdate() {
@@ -19,5 +29,20 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "Count: " + count.ToString ();
+		if(count >= 12) {
+			winText.text = "You win!";
+		}
 	}
 }
