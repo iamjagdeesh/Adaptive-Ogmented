fc72bb4a3d9bbdfefbf3e107b1dce40ec594076c
jagdeesh-basavaraju <jagdeesh.basavaraju@fireeye.com>
2018-11-11 06:38:27 -0700

    Added AR and movement through cellphone movement

--- Tutorials/Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Tutorials/Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -23,12 +23,35 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		//Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		rb.AddForce (movement * speed);
+		Vector3 dir = Vector3.zero;
+
+		// we assume that device is held parallel to the ground
+		// and Home button is in the right hand
+
+		// remap device acceleration axis to game coordinates:
+		//  1) XY plane of the device is mapped onto XZ plane
+		//  2) rotated 90 degrees around Y axis
+		dir.x = -Input.acceleration.y;
+		dir.z = Input.acceleration.x;
+
+		// clamp acceleration vector to unit sphere
+		if (dir.sqrMagnitude > 1)
+			dir.Normalize();
+
+		// Make it move 10 meters per second instead of 10 meters per frame...
+		dir *= Time.deltaTime;
+
+		// Move object
+		rb.transform.Translate(dir * speed);
+
+		//rb.AddForce(dir * speed);
+
+		//rb.AddForce (movement * speed);
 	}
 
 	void OnTriggerEnter(Collider other) {
@@ -41,7 +64,7 @@ public class PlayerController : MonoBehaviour {
 
 	void SetCountText() {
 		countText.text = "Count: " + count.ToString ();
-		if(count >= 12) {
+		if(count >= 5) {
 			winText.text = "You win!";
 		}
 	}
