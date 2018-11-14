d0f3577365e478fb95bdf51eca07bf8d0ed9f883
Aditya C <aditya.chayapathy@gmail.com>
2018-11-13 20:02:48 -0700

    generic coe

---
+++ Tutorials/Task 2/Assets/Scripts/LoginScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginScript : MonoBehaviour {
+
+	public InputField username;
+
+	public InputField password;
+
+	public GameObject loginObjectUI;
+
+	//public GameObject arObjects;
+
+	public GameObject userInfoUI;
+
+	public void Login () {
+		if (username.text.Equals ("jags") && password.text.Equals ("pwd")) {
+			Debug.Log ("Login Successful!");
+		}
+		loginObjectUI.SetActive (false);
+		DisplayUserInfoUI (username.text);
+		SceneManager.LoadScene ("Task 2");
+
+	}
+
+	public void DisplayUserInfoUI (string user) {
+		//Make a rest call here to fetch the details or send details from login()
+		userInfoUI.SetActive (true);
+		Text userName = userInfoUI.transform.GetChild (0).GetComponent<Text> ();
+		Text userID = userInfoUI.transform.GetChild (1).GetComponent<Text> ();
+		Text performance = userInfoUI.transform.GetChild (2).GetComponent<Text> ();
+		userName.text = "Jagdeesh Basavaraju";
+		userID.text = user;
+		performance.text = "Doing OK!";
+	}
+
+	public void populateStaticInfo () {
+		StaticGameInfo.userName = "Jagdeesh Basavaraju";
+		StaticGameInfo.userID = username.text;
+		StaticGameInfo.performance = "Doing OK!";
+	}
+}
---
+++ Tutorials/Task 2/Assets/Scripts/StaticGameInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticGameInfo {
+	public static string userName;
+	public static string userID;
+	public static string performance;
+	public static int currentTask;
+}
